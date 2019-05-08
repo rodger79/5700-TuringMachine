b3b0917118771d4fb75e8c7d3249ad796d5283a2
Rodger Byrd <rodger.byrd@gmail.com>
2019-05-08 11:22:33 -0600

    report updates

--- createteststrings.sh
+++ createteststrings.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-for i in {1..5}
+for i in {1..500}
+do
+   cat /dev/urandom | tr -dc 'a-e' | fold -w 5 | head -n 1 >> testfile_abcde_rand.txt
+done
+for i in {1..200}
+do
+   cat /dev/urandom | tr -dc 'a-e' | fold -w 10 | head -n 1 >> testfile_abcde_rand.txt
+done
+for i in {1..200}
+do
+   cat /dev/urandom | tr -dc 'a-e' | fold -w 15 | head -n 1 >> testfile_abcde_rand.txt
+done
+for i in {1..200}
+do
+   cat /dev/urandom | tr -dc 'a-e' | fold -w 20 | head -n 1 >> testfile_abcde_rand.txt
+done
+for i in {1..200}
+do
+   cat /dev/urandom | tr -dc 'a-e' | fold -w 25 | head -n 1 >> testfile_abcde_rand.txt
+done
+for i in {1..200}
 do
    cat /dev/urandom | tr -dc 'a-e' | fold -w 30 | head -n 1 >> testfile_abcde_rand.txt
 done
